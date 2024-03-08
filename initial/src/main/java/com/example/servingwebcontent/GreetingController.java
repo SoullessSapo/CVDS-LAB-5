e6bcb603e152a9c82c772e1f145a16d4f36fb8c6
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2024-03-08 16:20:23 -0500

    guess psot method

--- initial/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ initial/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
 
 @Controller
 public class GreetingController {
+    private int targetNumber;
+    private int balance = 100000;
+    private Random random = new Random();
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,4 +20,47 @@ public class GreetingController {
         return "greeting";
     }
 
+    import java.util.Random;
+
+    @Controller
+    public class GreetingController {
+        private int targetNumber;
+        private int balance = 100000;
+        private Random random = new Random();
+
+        public GreetingController() {
+            resetGame();
+        }
+
+        @GetMapping("/guess")
+        public String guessForm(Model model) {
+            model.addAttribute("balance", balance);
+            return "guess";
+        }
+
+        @PostMapping("/guess")
+        public String guess(@RequestParam(name="number", required=true) int number, Model model) {
+            if (number == targetNumber) {
+                model.addAttribute("result", "You guessed it!");
+                resetGame();
+            } else {
+                balance -= 10000;
+                model.addAttribute("result", "Try again!");
+            }
+            model.addAttribute("balance", balance);
+            return "guess";
+        }
+
+        @PostMapping("/reset")
+        public String reset(Model model) {
+            resetGame();
+            model.addAttribute("balance", balance);
+            return "guess";
+        }
+
+        private void resetGame() {
+            targetNumber = random.nextInt(10) + 1;
+            balance = 100000;
+        }
+    }
 }
