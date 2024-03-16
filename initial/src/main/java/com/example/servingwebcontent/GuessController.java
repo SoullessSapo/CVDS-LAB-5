e14558b1f9970c7e884d56162382d40df19398d6
Esteban Valencia Caicedo <93455764+SoullessSapo@users.noreply.github.com>
2024-03-15 21:24:31 -0500

    error grade al cambio de pc

---
+++ initial/src/main/java/com/example/servingwebcontent/GuessController.java
@@ -0,0 +1,51 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
+
+@Controller
+public class GuessController {
+    private int targetNumber;
+    private int balance = 100000;
+    private final Random random = new Random();
+
+    public GuessController() {
+        resetGame();
+    }
+
+    @GetMapping("/guess")
+    public String guessForm(Model model) {
+        model.addAttribute("balance", balance);
+        return "guess";
+    }
+
+    @PostMapping("/guess")
+    public String guess(@RequestParam(name="number", required=true) int number, Model model) {
+        if (number == targetNumber) {
+            model.addAttribute("result", "You guessed it!");
+            resetGame();
+        } else {
+            balance -= 10000;
+            model.addAttribute("result", "Try again!");
+        }
+        model.addAttribute("balance", balance);
+        return "guess";
+    }
+
+    @PostMapping("/reset")
+    public String reset(Model model) {
+        resetGame();
+        model.addAttribute("balance", balance);
+        return "guess";
+    }
+
+    private void resetGame() {
+        targetNumber = random.nextInt(10) + 1;
+        balance = 100000;
+    }
+}
